f8b0a23d3758a10a688f42447bd9915012fe1262
nicioniz <nicio.fava@gmail.com>
2018-07-04 16:16:13 +0200

    Refactoring resourceModel.pl.

--- src/sprint1/resourceModel.pl
+++ src/sprint1/resourceModel.pl
@@ -21,85 +21,59 @@ resource(name(robot), state(movement(stopped), obstacleDetected(false))).
  
 resource(name(temp), state(temperature(0))). 
 resource(name(timer), state(currentTime(hours(0), minutes(0), seconds(0)))).
- 
+
+resource(name(led), state(off)).
+
 resource(name(sonar1), state(somethingDetected(false), distance(0))).
 resource(name(sonar2), state(somethingDetected(false), distance(0))).
+
+resource(name(temperatureIsOk), state(false)).
+resource(name(timeIsOk), state(false)).
 
 getResource(NAME, STATE) :-
 		resource(name(NAME), STATE).
 
-
 changeModelItem(robot, movement(VALUE)) :-
 		resource(name(robot), state(movement(_), obstacleDetected(X))),
- 		(
-			retract(resource(name(robot), state(movement(_), obstacleDetected(X))));
-			true
-		),
- 		assert(resource(name(robot), state(movement(VALUE), obstacleDetected(X)))),
-		!,
-		emitevent(modelChanged, modelChanged(resource(name(robot), state(movement(VALUE), obstacleDetected(X))))),
-		(
-			changedModelAction(resource(name(robot), state(movement(VALUE), obstacleDetected(X)))) 	
-		 	; true								
-		).		
+		commonChangeModelItem(name(robot), state(movement(VALUE), obstacleDetected(X))).
+	
 
 
 changeModelItem(robot, obstacleDetected(VALUE)) :-
 		resource(name(robot), state(movement(X), obstacleDetected(_))),
-		(
-			retract(resource(name(robot), state(movement(X), obstacleDetected(_))));
-			true
-		),
- 		assert(resource(name(robot), state(movement(X), obstacleDetected(VALUE)))),
-		!,
-		emitevent(modelChanged, modelChanged(resource(name(robot), state(movement(X), obstacleDetected(VALUE))))),
-		(
-			changedModelAction(resource(name(robot), state(movement(X), obstacleDetected(VALUE)))) 	
-		 	; true								
-		).
+		commonChangeModelItem(name(robot), state(movement(X), obstacleDetected(VALUE))).
 
 
 changeModelItem(NAME, turnLed(VALUE)) :-
-		resource(name(NAME), state(OLD_STATE)),
- 		(
-			retract(resource(name(NAME), state(OLD_STATE)));
-			true
-		),
- 		assert(resource(name(NAME), state(VALUE))),
-		!,
-		emitevent(modelChanged, modelChanged(resource(name(NAME), state(VALUE)))),
-		(
-			changedModelAction(resource(name(NAME), state(VALUE))) 	
-		 	; true								
-		).
+ 		commonChangeModelItem(name(NAME), state(VALUE)).
 		
 changeModelItem(temp, updateTemperature(VALUE)) :-
-		resource(name(temp), state(OLD_STATE)),
- 		(
-			retract(resource(name(temp), state(OLD_STATE)));
-			true
-		),
- 		assert(resource(name(temp), state(temperature(VALUE)))),
-		!,
-		emitevent(modelChanged, modelChanged(resource(name(temp), state(temperature(VALUE))))),
-		(
-			changedModelAction(resource(name(temp), state(temperature(VALUE)))) 	
-		 	; true								
-		).
+ 		commonChangeModelItem(name(temp), state(temperature(VALUE))).
 		
 changeModelItem(timer, updateTime(currentTime(hours(HOURS), minutes(MINUTES), seconds(SECONDS)))) :-
-		resource(name(timer), state(OLD_STATE)),
- 		(
-			retract(resource(name(timer), state(OLD_STATE)));
+ 		commonChangeModelItem(name(timer), state(currentTime(hours(HOURS), minutes(MINUTES), seconds(SECONDS)))).
+		
+changeModelItem(temperatureIsOk, STATE) :- 		
+		commonChangeModelItem(name(temperatureIsOk), state(STATE)).
+ 		
+
+changeModelItem(timeIsOk, STATE) :-
+ 		commonChangeModelItem(name(timeIsOk), state(STATE)).
+ 		
+
+commonChangeModelItem(NAME, STATE) :- 
+		(
+			retract(resource(NAME, _));
 			true
 		),
- 		assert(resource(name(timer), state(currentTime(hours(HOURS), minutes(MINUTES), seconds(SECONDS))))),
+ 		assert(resource(NAME, STATE)),
 		!,
-		emitevent(modelChanged, modelChanged(resource(name(timer), state(currentTime(hours(HOURS), minutes(MINUTES), seconds(SECONDS)))))),
+		emitevent(modelChanged, modelChanged(resource(NAME, STATE))),
 		(
-			changedModelAction(resource(name(timer), state(currentTime(hours(HOURS), minutes(MINUTES), seconds(SECONDS))))) 	
+			changedModelAction(resource(NAME, STATE)) 	
 		 	; true								
 		).
+
 
 eval( ge, X, X ) :- !. 
 eval( ge, X, V ) :- eval( gt, X , V ) .
